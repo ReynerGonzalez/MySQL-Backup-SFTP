da679e223018d972ee849678baea8711e030252f
Reyner González <dev@reynergonzalez.com>
2021-03-08 09:55:50 -0600

    Commit inicial
    Funcionalidad base para la creación de los respaldos

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# mysql-backup-sftp
-Backup Script for MySQL and SFTP
+# MySQL Backup SFTP
+
+Un script para realizar respaldos de MySQL a un servidor externo por medio de FTP o SFTP
+
+## Instalación
+
+### Instale en el servidor origen [rsync](https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/selinux_users_and_administrators_guide/chap-managing_confined_services-rsync) 
+
+```bash
+sudo yum install rsync
+```
+
+### Instale en el servidor destino [rsync](https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/selinux_users_and_administrators_guide/chap-managing_confined_services-rsync) 
+
+```bash
+sudo yum install rsync
+```
+
+### Complete los valores de las variables
+
+En el archivo mysql_backup_sftp.sh
+
+### Brinde permisos de ejecución al script
+
+```bash
+sudo chmod +x mysql_backup_sftp.sh
+```
+
+## Uso
+
+Ejecute el archivo
+
+```bash
+sh mysql_backup_sftp.sh
+```
+
+## Contribuir
+Solicitudes Pull son bienvenidas. Para cambios mayores, por favor abra un caso primero para conversar acerca del cambio que se desea realizar.
+
+Por favor asegurese de tener la versión actualizada
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ mysql-backup-sftp.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Linux MySQL Database SFTP Backup Script
+# Version: 1.0
+# Script by: Reyner González
+
+backup_path="./backups"
+
+create_backup() {
+  umask 177
+
+  date=$(date '+%y%m%d')
+  name="client-name-db_"
+  FILE="$name$date.sql"
+  mysqldump --user=$user --password=$password --host=$host $db_name > $FILE
+
+  echo 'Backup Complete'
+}
+
+clean_backup() {
+  rm -f $backup_path/$FILE
+  echo 'Local Backup Removed'
+}
+
+########################
+# Edit Below This Line #
+########################
+
+# Database credentials
+
+user="USERNAME HERE"
+password="PASSWORD HERE"
+host="IP HERE"
+db_name="DATABASE NAME HERE"
+
+# FTP Login Data
+USERNAME="USERNAME HERE"
+PASSWORD="PASSWORD HERE"
+SERVER="IP HERE"
+PORT="SERVER PORT HERE"
+
+#Remote directory where the backup will be placed
+REMOTEDIR="./"
+
+#Transfer type
+#1=FTP
+#2=SFTP
+TYPE=2
+
+##############################
+# Don't Edit Below This Line #
+##############################
+
+cd $backup_path
+create_backup
+
+if [ $TYPE -eq 1 ]
+then
+ftp -n -i $SERVER <<EOF
+user $USERNAME $PASSWORD
+binary
+cd $REMOTEDIR
+mput $FILE
+quit
+EOF
+elif [ $TYPE -eq 2 ]
+then
+rsync --rsh="sshpass -p $PASSWORD ssh -p $PORT -o StrictHostKeyChecking=no -l $USERNAME" $backup_path/$FILE $SERVER:$REMOTEDIR
+else
+echo 'Please select a valid type'
+fi
+
+echo 'Remote Backup Complete'
+clean_backup
+#END
